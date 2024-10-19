3860146061c3815325cd42fcb7c89082d00da203
yashwant432 <97020618+yashwant432@users.noreply.github.com>
2024-10-19 15:07:44 +0530

    Create ConvertSpacesToUnderscores.java

---
+++ ConvertSpacesToUnderscores.java
@@ -0,0 +1,12 @@
+package harry;
+
+public class ConvertSpacesToUnderscores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "I am Varun Patil";
+		str = str.replaceAll(" ", "_");
+		System.out.println(str);
+	}
+
+}
